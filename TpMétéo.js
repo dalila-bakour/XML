1bc37621ab0767eaf5e948a19e37405600b95f5b
dalila-bakour <78749703+dalila-bakour@users.noreply.github.com>
2021-02-10 20:55:25 +0100

    Add files via upload

---
+++ TpMétéo.js
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>  
+        <meta charset="UTF-8">
+        <title>TP XML Metéo</title>
+        <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <section class=" header">
+          <div>
+              <h1><Span>TP XML Metéo HTML</Span></h1>
+           </div>
+    </section>
+    <section class="grille">
+        <div class="container">
+            <div class="ville" id="69">
+                <div class="title">
+                     <h2> <strong> LYON </strong>  </h2>
+                     <img src="noun_sun_cloud_653891.png" alt="Ensoleillé dans l'ensemble"/>
+                </div>   
+                <div class="temps">
+                     <p>25C°, Ensoleillé.</p>
+                 </div>    
+            </div>
+            <div class="ville" id="75">
+                <div class="title">
+                     <h2> <strong> PARIS </strong>  </h2>
+                     <img src="noun_Partly_Sunny_Cloud_655900.png" alt="Partiellement couvert"/>
+                </div>   
+                <div class="temps">
+                     <p>10C°, Couvert.</p>
+                 </div>    
+            </div><div class="ville" id="67">
+                <div class="title" >
+                     <h2> <strong> STRASBOURG </strong>  </h2>
+                     <img src="noun_clouds_653375.png" alt="Nuageux"/>
+                </div>   
+                <div class="temps">
+                     <p>5C°, Nuageux.</p>
+                 </div>    
+            </div>
+          </div>
+    </section>
+    <section class="bas" >
+        <div class="footer">
+            <h4 > DALILA   BAKOUR  (11716414) <br> Icones par :
+                <a href="https://thenounproject.com/term/raindrop-cloud/653383/">Linseed Studio, US</a>
+              </h4>
+        </div>
+    </section>
+    <script>
+        function showCity() 
+            {
+                let nomVilleSelectionnee = document.getElementById(ID-DE-VOTE-MENU).value;
+                let cities = document.getElementsByClassName("city");
+                for (i=0; i<l; i++) 
+                    {
+                    if (citis[i].id == NOM-ville-SELECTIONNEE) 
+                      {
+     
+                      }
+                    else 
+                       {
+     
+                       }
+            }
+    </script>
+  </body>
+</html>
